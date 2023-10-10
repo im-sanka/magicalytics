fd6252f20e3458afd1cd3bc1904000287a170577
sanka <immanuel.sanka@taltech.ee>
2023-10-10 22:22:11 +0300

    revising the extract_.py to pull only one data

--- src/etl/extract_.py
+++ src/etl/extract_.py
@@ -32,19 +32,6 @@ def add_data_to_duckdb(df, table_schema):
     db_path = os.path.join(folder_path, 'test_data.duckdb')
     con = duckdb.connect(database=db_path, read_only=False)
 
-    unique_ranks = df['rank'].unique()
-
-    for rank in unique_ranks:
-        table_name = f"google_trends_data_rank_{rank}"
-        create_table_sql = f"""
-            CREATE TABLE IF NOT EXISTS {table_name} {table_schema}
-        """
-        con.execute(create_table_sql)
-
-        chunk = df[df['rank'] == rank]
-        con.register('temp_chunk', chunk)
-        con.execute(f"INSERT INTO {table_name} SELECT * FROM temp_chunk")
-
     return con
 
 if __name__ == "__main__":
@@ -71,7 +58,4 @@ if __name__ == "__main__":
     """
 
     con = add_data_to_duckdb(df, table_schema)
-
-    # Just checking the table in the database
-    result = con.execute("SELECT * FROM google_trends_data_rank_1").fetchdf()
-    print(result.head())
+    print("Query done!")
